c7d4214829c09eec0ab8a5fe2d827681537cb335
koany <koroliev@yandex.ru>
2023-07-16 15:31:42 +0300

    сделан баскетбол

--- basketball.py
+++ basketball.py
@@ -1,57 +1,96 @@
-class BookShelf:
+class BasketballAssociation:
     def __init__(self):
-        self.books = []
+        self.players = []
 
-    def add(self, title, year):
-        self.books.append(Book(title, int(year)))
+    def find(self, text):
+        nums = []
+        finded = []
+        for i, p in enumerate(self.players):
+            if p.name == text or str(p.height) == text or p.club == text:
+                nums.append(i)
+                finded.append(p)
+        return nums, finded
 
-    def sortbytitle(self):
-        self.books.sort(key=lambda x: x.title)
+    def showfinded(self, text):
+        nums, finded = self.find(text)
+        if finded:
+            self.showplayers(finded, nums)
+        else:
+            print('Не найдено')
 
-    def sortbyyear(self):
-        self.books.sort(key=lambda x: x.year)
+    def add(self, name, height, club):
+        self.players.append(Player(name, int(height), club))
 
-    def showbooks(self):
-        print('-------------------')
-        print('Название       Год')
-        print('-------------------')
-        for i in self.books:
-            i.showbook()
-        print('-------------------')
+    def remove(self, num):
+        self.players.pop(num - 1)
+
+    def replace(self, num, what, text):
+        if what == 1:
+            self.players[num - 1].name = text
+        elif what == 2:
+            self.players[num - 1].height = int(text)
+        elif what == 3:
+            self.players[num - 1].club = text
+
+    def showplayers(self, players, nums=[]):
+        if not nums:
+            nums = [i for i in range(len(players))]
+        print('-------------------------------------------')
+        print('№  Имя            Рост      Клуб           ')
+        print('-------------------------------------------')
+        for i, p in enumerate(players):
+            print(str(nums[i] + 1).ljust(3), end='')
+            p.showplayer()
+        print('-------------------------------------------')
 
     def menu(self):
         ans = int(input('Выберите действие:\n'
-                        '\t1 - Добавить книгу\n'
-                        '\t2 - Отсортировать по названию\n'
-                        '\t3 - Отсортировать по году\n'
-                        '\t4 - Вывести список книг\n'
+                        '\t1 - Добавить игрока\n'
+                        '\t2 - Удалить игрока\n'
+                        '\t3 - Найти игроков\n'
+                        '\t4 - Замена данных\n'
+                        '\t5 - Вывод списка игроков\n'
                         '\t0 - Выход\n'))
         if ans == 1:
-            title = input('Введите название:\n')
-            year = input('Введите год:\n')
-            self.add(title, year)
+            name = input('Введите имя:\n')
+            height = input('Введите рост:\n')
+            club = input('Введите клуб:\n')
+            self.add(name, height, club)
         elif ans == 2:
-            self.sortbytitle()
+            num = int(input('Введите номер игрока для удаления:\n'))
+            self.remove(num)
         elif ans == 3:
-            self.sortbyyear()
+            text = input('Введите имя, рост или клуб для поиска:\n')
+            self.showfinded(text)
         elif ans == 4:
-            self.showbooks()
+            num = int(input('Введите номер игрока для изменения данных:\n'))
+            what = int(input('Что будем менять?\n'
+                             '\t1 - Имя\n'
+                             '\t2 - Рост\n'
+                             '\t3 - Клуб\n'))
+            text = input('На что будем менять?\n')
+            self.replace(num, what, text)
+        elif ans == 5:
+            self.showplayers(self.players)
         elif ans == 0:
             return
         self.menu()
 
 
-class Book:
-    def __init__(self, title, year):
-        self.title = title
-        self.year = year
+class Player:
+    def __init__(self, name, height, club):
+        self.name = name
+        self.height = height
+        self.club = club
 
-    def showbook(self):
-        print(self.title.ljust(14), self.year)
+    def showplayer(self):
+        print(self.name.ljust(14), str(self.height).ljust(9), self.club.ljust(14))
 
 
-b = BookShelf()
-b.add('ванп', 1992)
-b.add('абнп', 1991)
-b.add('аанп', 1998)
-b.menu()
+nba = BasketballAssociation()
+nba.add('Вася', 175, 'Клуб 1')
+nba.add('Петя', 185, 'Клуб 3')
+nba.add('Саша', 179, 'Клуб 1')
+nba.add('Стас', 175, 'Клуб 2')
+nba.add('Гена', 195, 'Клуб 2')
+nba.menu()
